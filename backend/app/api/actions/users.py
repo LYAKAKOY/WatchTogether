6fe3081c4866d63ffd89754edd2ff6eb917f87de
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-19 02:42:24 +0300

    refactoring code

--- backend/app/api/actions/users.py
+++ backend/app/api/actions/users.py
@@ -1,7 +1,7 @@
 import uuid
+
 from api.users.schemas import CreateUser
 from api.users.schemas import ShowUser
-from api.users.schemas import UpdateUser
 from api.users.schemas import UpdatePasswordUser
 from db.users.dals import UserDAL
 from hashing import Hasher
@@ -15,11 +15,17 @@ async def _create_user(body: CreateUser, session: AsyncSession) -> ShowUser | No
             login=body.login, password=Hasher.get_password_hash(body.password)
         )
         if new_user is not None:
-            return ShowUser(user_id=new_user.user_id, email=new_user.nickname,
-                    nickname=new_user.nickname, avatar=new_user.avatar)
+            return ShowUser(
+                user_id=new_user.user_id,
+                email=new_user.nickname,
+                nickname=new_user.nickname,
+                avatar=new_user.avatar,
+            )
 
 
-async def _update_user_password(body: UpdatePasswordUser, session: AsyncSession) -> ShowUser | None:
+async def _update_user_password(
+    body: UpdatePasswordUser, session: AsyncSession
+) -> ShowUser | None:
     async with session.begin():
         user_dal = UserDAL(session)
         user = await user_dal.set_password(
@@ -28,10 +34,17 @@ async def _update_user_password(body: UpdatePasswordUser, session: AsyncSession)
         if user is not None:
             return ShowUser(user_id=user.user_id)
 
-async def _update_user(user_id: uuid.UUID, update_user_data: dict, session: AsyncSession) -> ShowUser | None:
+
+async def _update_user(
+    user_id: uuid.UUID, update_user_data: dict, session: AsyncSession
+) -> ShowUser | None:
     async with session.begin():
         user_dal = UserDAL(session)
         user = await user_dal.update_user(user_id=user_id, **update_user_data)
         if user is not None:
-            return ShowUser(user_id=user.user_id, email=user.nickname,
-                    nickname=user.nickname, avatar=user.avatar)
+            return ShowUser(
+                user_id=user.user_id,
+                email=user.email,
+                nickname=user.nickname,
+                avatar=user.avatar,
+            )
--- backend/app/api/users/schemas.py
+++ backend/app/api/users/schemas.py
@@ -1,7 +1,9 @@
 import uuid
 
-from fastapi import HTTPException, UploadFile
-from pydantic import BaseModel, field_validator
+from fastapi import HTTPException
+from fastapi import UploadFile
+from pydantic import BaseModel
+from pydantic import field_validator
 
 
 class TunedModel(BaseModel):
@@ -16,23 +18,21 @@ class CreateUser(BaseModel):
     @field_validator("login")
     def validate_name(cls, value):
         if not len(value) > 5:
-            raise HTTPException(
-                status_code=422, detail="login is too short"
-            )
+            raise HTTPException(status_code=422, detail="login is too short")
         return value
 
     @field_validator("password")
     def validate_surname(cls, value):
         if not len(value) > 5:
-            raise HTTPException(
-                status_code=422, detail="password is too easy"
-            )
+            raise HTTPException(status_code=422, detail="password is too easy")
         return value
+
 
 class UpdateUser(BaseModel):
     nickname: str | None
     email: str | None
-    avatar: UploadFile | None
+    avatar: UploadFile | None = None
+
 
 class UpdatePasswordUser(BaseModel):
     login: str
@@ -41,9 +41,7 @@ class UpdatePasswordUser(BaseModel):
     @field_validator("password")
     def validate_surname(cls, value):
         if not len(value) > 5:
-            raise HTTPException(
-                status_code=422, detail="password is too easy"
-            )
+            raise HTTPException(status_code=422, detail="password is too easy")
         return value
 
 
